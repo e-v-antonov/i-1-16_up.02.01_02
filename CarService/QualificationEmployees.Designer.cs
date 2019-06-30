0443b66191c8a28e523c2c992db0eba99647ba66
vityaapx500 <vityaapx500@gmail.com>
2019-06-30 23:35:14 +0300

    Изменил размер формы Квалификация

--- CarService/QualificationEmployees.Designer.cs
+++ CarService/QualificationEmployees.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // QualificationEmployees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(494, 497);
+            this.Name = "QualificationEmployees";
             this.Text = "QualificationEmployees";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
